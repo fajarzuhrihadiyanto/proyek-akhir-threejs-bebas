3dfecf4e34498e1ef75f1183d416413cebfbf576
Fajar Zuhri H <fajarzuhri76@gmail.com>
2022-11-29 21:07:22 +0700

    feat: add flag component

--- src/components/universe/earth/points/flag/index.jsx
+++ src/components/universe/earth/points/flag/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import * as THREE from 'three';
+import {useLoader} from "@react-three/fiber";
+import {PointContext} from "../Point";
 
 const Flag = React.forwardRef((_, ref) => {
+  const {countryCode} = React.useContext(PointContext)
+  const map = useLoader(THREE.TextureLoader, `https://flagcdn.com/w320/${countryCode}.png`)
   return (
     <group ref={ref}>
       <mesh>
         <cylinderGeometry args={[.005, .005, .5, 20]}/>
         <meshStandardMaterial color='#FFFFFF'/>
+      </mesh>
+      <mesh position={[0.075,0.2125,0]}>
+        <planeGeometry args={[.15,.075]}/>
+        <meshStandardMaterial color='#FFFFFF' side={THREE.DoubleSide} map={map}/>
       </mesh>
     </group>
   )
--- src/constant/points.js
+++ src/constant/points.js
@@ -1,11 +1,13 @@
 export default [
   {
+    countryCode: 'id',
     coordinate: [-6.2088, 106.8456],
     modelName: 'monumen_nasional.glb',
     fullModelScale: 0.08,
     altitude: .03
   },
   {
+    countryCode: 'fr',
     coordinate: [48.8566, 2.3522],
     modelName: 'eiffel_tower.glb',
     fullModelScale: 0.02,
