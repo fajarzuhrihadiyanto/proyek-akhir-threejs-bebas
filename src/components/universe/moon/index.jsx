7cb4d81aafb0d738cdc7ad121d121971b780002a
christhopermarcelino <christhopermarcelino@gmail.com>
2022-11-30 10:00:31 +0700

    refactor: implement moon revolution using useFrame

--- src/components/universe/moon/index.jsx
+++ src/components/universe/moon/index.jsx
@@ -1,28 +1,30 @@
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 
 const Moon = () => {
+  const ref = useRef();
+  const [incrementX, setIncrementX] = useState(true);
   const moonMap = useLoader(TextureLoader, "textures/moon.jpg");
 
-  const [rotation, setRotation] = useState([0, 0, 0]);
   const position = [0, 0, -10];
   const radius = 0.75;
   const widthSegments = 64;
   const heightSegments = 64;
 
-  useEffect(() => {
-    let [x, y, z] = rotation;
+  useFrame(() => {
+    if (ref.current.rotation.x >= 0.3) setIncrementX(false);
+    else if (ref.current.rotation.x <= -0.3) setIncrementX(true);
 
-    setInterval(() => {
-      x += 0.0005;
-      z += 0.0005;
-      setRotation([x, y, z]);
-    }, 100);
-  }, []);
+    incrementX
+      ? (ref.current.rotation.x += 0.005)
+      : (ref.current.rotation.x -= 0.005);
+
+    ref.current.rotation.y += 0.05;
+  });
 
   return (
-    <group rotation={rotation}>
+    <group ref={ref}>
       <mesh position={position}>
         <sphereGeometry args={[radius, widthSegments, heightSegments]} />
         <meshPhongMaterial map={moonMap} />
