027c7c4effbffb3299fcb7914be452516b7a26b8
Fajar Zuhri H <fajarzuhri76@gmail.com>
2022-12-10 22:08:48 +0700

    feat: change static population graph to dynamic population graph using api

--- src/components/universe/earth/points/population/index.jsx
+++ src/components/universe/earth/points/population/index.jsx
@@ -2,43 +2,45 @@ import React, {forwardRef} from "react";
 import * as THREE from 'three'
 
 import Bar from "./Bar";
+import {PointContext} from "../Point";
 
 const Population = forwardRef((_, ref) => {
+  const {code} = React.useContext(PointContext)
 
   const width = 0.05
   const max_height = 0.5
+  const min_height = 0.2
+  const range = max_height - min_height
 
-  const data = [
-    {
-      year: 2017,
-      pop: 450000
-    },
-    {
-      year: 2018,
-      pop: 625000
-    },
-    {
-      year: 2019,
-      pop: 775000
-    },
-    {
-      year: 2020,
-      pop: 900000
-    },
-    {
-      year: 2021,
-      pop: 1000000
-    }
-  ]
+  const [data, setData] = React.useState([])
+
   const max_population = Math.max(...data.map(e => e.pop))
+  const min_population = Math.min(...data.map(e => e.pop))
+  const range_population = max_population - min_population
 
   data.forEach((e, index) => {
     e.x = (index - (data.length - 1) / 2) * width
-    e.height = e.pop * max_height / max_population
+    e.height = ((e.pop - min_population) * range / range_population) + min_height
 
     const colorDiff = 360 / data.length
     e.color = `#${(new THREE.Color(`hsl(${index * colorDiff}, 100%, 50%)`)).getHexString()}`
   })
+
+  const getData = async () => {
+    const res = await fetch(`https://api.worldbank.org/v2/country/${code}/indicator/SP.POP.TOTL?date=2017:2021&format=json`)
+    const data = await res.json()
+
+    const populations = data[1].map(e => ({
+      year: e.date,
+      pop: e.value
+    })).reverse()
+
+    setData(populations)
+  }
+
+  React.useEffect(() => {
+    getData()
+  }, [])
 
   return (
     <group ref={ref}>
@@ -46,26 +48,6 @@ const Population = forwardRef((_, ref) => {
       {data.map(e => (
         <Bar year={e.year} pop={e.pop} height={e.height} x={e.x} color={e.color}/>
       ))}
-      {/*<mesh position={[.06,0,0]}>*/}
-      {/*  <boxGeometry args={[.03,.6,.03]}/>*/}
-      {/*  <meshStandardMaterial color='#FF0000'/>*/}
-      {/*</mesh>*/}
-      {/*<mesh position={[.03,0,0]}>*/}
-      {/*  <boxGeometry args={[.03,.5,.03]}/>*/}
-      {/*  <meshStandardMaterial color='#FF8800'/>*/}
-      {/*</mesh>*/}
-      {/*<mesh position={[0,0,0]}>*/}
-      {/*  <boxGeometry args={[.03,.4,.03]}/>*/}
-      {/*  <meshStandardMaterial color='#FFFF00'/>*/}
-      {/*</mesh>*/}
-      {/*<mesh position={[-.03,0,0]}>*/}
-      {/*  <boxGeometry args={[.03,.3,.03]}/>*/}
-      {/*  <meshStandardMaterial color='#88FF00'/>*/}
-      {/*</mesh>*/}
-      {/*<mesh position={[-.06,0,0]}>*/}
-      {/*  <boxGeometry args={[.03,.2,.03]}/>*/}
-      {/*  <meshStandardMaterial color='#00FF00'/>*/}
-      {/*</mesh>*/}
     </group>
   )
 })
